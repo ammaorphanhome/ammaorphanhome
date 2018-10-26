7dc390c1b767519418fbabf96c19bb9209e836a1
ammaorphanhome <ammaorphanhome@gmail.com>
2018-10-26 19:40:44 -0400

    Fix for menu highlight issue

--- payment.php
+++ payment.php
@@ -12,14 +12,14 @@ $ch = curl_init();
 // 'test_5f4292c874a5f602a115c0aa965', 'test_7aac381962a51a81aae0776e7c7','https://test.instamojo.com/api/1.1/'
 // 19bf1495dabe8eee14c01fbd14eb3aec , c40bbd668ead7811ac67465f01a8cc09
 
-curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/payment-requests/');
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER,
-    array("X-Api-Key:your-key",
-        "X-Auth-Token:your-token"));
-    
+    curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/payment-requests/');
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,
+        array("X-Api-Key:test_5f4292c874a5f602a115c0aa965",
+            "X-Auth-Token:test_7aac381962a51a81aae0776e7c7"));
+
    
     $fullname = $info['name'];
     $amount = $info['price']; 
--- test_payment.php
+++ test_payment.php
@@ -9,9 +9,6 @@ if($info = $data->fetch()) {
 
     $ch = curl_init();
 
-// 'test_5f4292c874a5f602a115c0aa965', 'test_7aac381962a51a81aae0776e7c7','https://test.instamojo.com/api/1.1/'
-// 19bf1495dabe8eee14c01fbd14eb3aec , c40bbd668ead7811ac67465f01a8cc09
-
     curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
     curl_setopt($ch, CURLOPT_HEADER, FALSE);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
