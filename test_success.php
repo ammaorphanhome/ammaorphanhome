550c298e36dfbcd726aa37651c597a0ac7bf6d73
ammaorphanhome <ammaorphanhome@gmail.com>
2018-10-27 00:38:13 -0400

    Footer changes
    Footer changes

--- test_success.php
+++ test_success.php
@@ -131,65 +131,12 @@ require "cw_admin/lib/config.php";
             </div>
         </section>
 
-        <footer class="probootstrap-footer probootstrap-bg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 probootstrap-animate">
-                        <div class="probootstrap-footer-widget">
-                            <h3>About Us</h3>
-                            <p>Amma Orphanage, Known as the "Amma Anadha Saranalayam" is a non profit organisation Founded by Mr. E. Teja with the objective to provide solution to the orphans.</p>
-                            <ul class="probootstrap-footer-social">
-                                <li><a href="#"><i class="icon-twitter"></i></a></li>
-                                <li><a href="#"><i class="icon-facebook"></i></a></li>
-                                <li><a href="#"><i class="icon-linkedin"></i></a></li>
-                                <li><a href="#"><i class="icon-youtube"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 probootstrap-animate">
-                        <div class="probootstrap-footer-widget">
-                            <h3>Contact Info</h3>
-                            <ul class="probootstrap-contact-info">
-                                <li><i class="icon-location2"></i> <span> #4-133, Hemachandrapuram,
-                                    Karukonda Gram Panchayat,
-                                    Kothagudem, Telangana,
-                                    India.</span></li>
-                                <li><i class="icon-mail"></i><span><a href="#" class="__cf_email__"> info@ammaorphanhome.org</a></span></li>
-                                <li><i class="icon-phone2"></i><span> +91 87907 82983</span></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 probootstrap-animate">
-                        <div class="probootstrap-footer-widget">
-                            <h3>Location</h3>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121747.16400080919!2d80.545156332736!3d17.526651455912013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3410a556a5bd1d%3A0x9716a9866e772f09!2sKothagudem%2C+Telangana!5e0!3m2!1sen!2sin!4v1537863488574" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
-                    </div>
-
-                </div>
-                <!-- END row -->
-
-            </div>
-
-            <div class="probootstrap-copyright">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 text-left">
-                            <p>&copy; 2018 <a href="#">Amma Orphanage</a>. All Rights Reserved.</p>
-                        </div>
-                        <div class="col-md-4 probootstrap-back-to-top">
-                            <p><a href="#" class="js-backtotop">Back to top <i class="icon-arrow-long-up"></i></a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include('footer.php');?>
 
         <script src="js/scripts.min.js"></script>
         <script src="js/main.min.js"></script>
         <script src="js/custom.js"></script>
+        <script src="js/highlight.js"></script>
 
     </body>
 </html>
